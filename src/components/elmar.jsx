c56c4778058c7d738c90312d17ab3eafcec8136d
Elmar246 <elmarchik2007@gmail.com>
2025-10-11 10:11:04 +0600

    elmar
    elmar

---
+++ src/components/elmar.jsx
@@ -0,0 +1,12 @@
+function elmar() {
+  return (
+    <div className="student-card">
+      <h2>Elmar Kalmukhamedov</h2>
+      <p>Email: elmarchik2007@gmail.com</p>
+      <p>Phone: +996700480004</p>
+      <p>Course: Computer Science</p>
+    </div>
+  );
+}
+
+export default elmar;
