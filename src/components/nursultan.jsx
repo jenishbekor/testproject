587b196c5dd7fdde0dffca69eac294bd969110f5
NURSULTAN00766 <nursiksyrtbaev07@gmail.com>
2025-10-11 10:24:06 +0600

    nursultan
    created jsx

---
+++ src/components/nursultan.jsx
@@ -0,0 +1,12 @@
+function nursultan() {
+  return (
+    <div className="student-card">
+      <h2>Nursultan</h2>
+      <p>Email: nursiksyrtbaev07@gmail.com</p>
+      <p>Phone: +996709880098</p>
+      <p>Course: csc23 </p>
+    </div>
+  );
+}
+
+export default nursultan;
