85c423a400558f7f5cdc331fcb05e37cd5b57f0f
jenishbekor <57383078+jenishbekor@users.noreply.github.com>
2025-10-11 09:37:30 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Zhenish from "./components/Zhenish";
+
+function App() {
+  return (
+    <div>
+      <h1>Student Directory</h1>
+      <div className="student-list">
+        <Zhenish />
+
+        {/* add more student components here */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Zhenish.jsx
@@ -0,0 +1,12 @@
+function Zhenish() {
+  return (
+    <div className="student-card">
+      <h2>Zhenishbek Orozakhunov</h2>
+      <p>Email: zhenishbek@example.com</p>
+      <p>Phone: +996777123456</p>
+      <p>Course: Computer Science</p>
+    </div>
+  );
+}
+
+export default Zhenish;
