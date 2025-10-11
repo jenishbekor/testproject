da941017c3e2c9daaa5faee2db1c114c075969ee
lkdeous <a_albina0@mail.ru>
2025-10-11 10:12:54 +0600

    albina
    albina

---
+++ src/components/albina.jsx
@@ -0,0 +1,12 @@
+function albina() {
+  return (
+    <div className="student-card">
+      <h2>Albina Asrankulova</h2>
+      <p>Email: albina@example.com</p>
+      <p>Phone: +9969487987</p>
+      <p>Course: Computer Science</p>
+    </div>
+  );
+}
+
+export default albina;
