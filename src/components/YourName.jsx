b6091f780df3f0497afb3bb214ec1e94b48e7642
jenishbekor <57383078+jenishbekor@users.noreply.github.com>
2025-10-11 10:11:13 +0600

    Zhenishbek
    Zhenishbek

---
+++ src/components/YourName.jsx
@@ -0,0 +1,12 @@
+function YourName() {
+  return (
+    <div className="student-card">
+      <h2>Name Surname</h2>
+      <p>Email: test@example.com</p>
+      <p>Phone: +9968347847</p>
+      <p>Course: Computer Science</p>
+    </div>
+  );
+}
+
+export default Zhenish;
