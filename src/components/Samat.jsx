132b7b825731c2f853d1202c2a926be8be9246f2
NamaeNoNai <hitobitoochitsuite@gmail.com>
2025-10-11 10:14:36 +0600

    Samat changes something

--- src/components/Samat.jsx
+++ src/components/Samat.jsx
@@ -3,7 +3,7 @@ function Samat() {
     <div className="student-card">
       <h2>Samat Dzholoev</h2>
       <p>Email: samat@example.com</p>
-      <p>Phone: +996775839099</p>
+      <p>Phone: +996543679</p>
       <p>Course: Computer Science</p>
     </div>
   );
