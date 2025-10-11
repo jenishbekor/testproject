0fbc04fe9cac1d3a5b2f7fd201ab99cc52593809
jenishbekor <57383078+jenishbekor@users.noreply.github.com>
2025-10-11 10:22:22 +0600

    second changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import Zhenish from "./components/Zhenish";
+import Albina from "./components/albina";
+import Nurjanat from "./components/Nurjanat";
+import Samat from "./components/Samat";
 
 function App() {
   return (
@@ -6,6 +9,9 @@ function App() {
       <h1>Student Directory</h1>
       <div className="student-list">
         <Zhenish />
+        <Albina />
+        <Nurjanat />
+        <Samat />
 
         {/* add more student components here */}
       </div>
