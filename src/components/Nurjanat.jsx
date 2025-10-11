8f8fa702d704a661893da298e58848a7de1ac8e7
chanchin <164968720+nurjanatsca23@users.noreply.github.com>
2025-10-11 10:10:58 +0600

    Nurjanat
    Nurjanat

---
+++ src/components/Nurjanat.jsx
@@ -0,0 +1,12 @@
+function Nurjanat() {
+  return (
+    <div className="student-card">
+      <h2>Sultanova Nurjanat</h2>
+      <p>Email: boystorylove0@gmail.com</p>
+      <p>Phone: +996777123456</p>
+      <p>Course: Computer Science</p>
+    </div>
+  );
+}
+
+export default Nurjanat;
